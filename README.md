93ff8b8ae283408dba85ce6390bb43c492b10bdc
Timothy von Hollen <timothyvonhollen@gmail.com>
2022-08-04 21:28:17 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# This is a boilerplate for a Node.js app using Typescript.
+
+## Running this project
+First install the dependencies using `npm install` and then `npm run watch` for changes to be output into the build\src directory. Then run the app using `node main.js`. 
---
+++ jest.config.js
@@ -0,0 +1,22 @@
+export default {
+    preset: 'ts-jest/presets/js-with-ts',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    globals: {
+      'ts-jest': {
+        useESM: true,
+      },
+    },
+    moduleNameMapper: {
+      '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transform: {
+      "^.+\\.ts$": "ts-jest"
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+      'src/**/*.ts',
+      '!src/**/*.d.ts',
+    ],
+  };
